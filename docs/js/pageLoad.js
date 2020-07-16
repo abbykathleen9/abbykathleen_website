435dace4545b318da870a4e70abc08a787cb6d39
SHUTitDOWN1998 <SHUTitDOWN1998@gmail.com>
2020-07-16 06:02:24 -0500

    Summer Update - 7-16-2020
    Added rustic paintings and updated footer / nav importing from template.

---
+++ docs/js/pageLoad.js
@@ -0,0 +1,50 @@
+$().ready(function () {
+
+
+    
+
+
+
+    //IMPORT FOOTER
+    $.ajax({
+        type: "GET",
+        url: "./template/websiteLogo.html",
+        async: false,
+        success: function(text) {
+            $("head").append(text);
+        }
+    })
+    $("footer").load("./template/footer.html");
+    $("nav").load("./template/nav.html", function () {
+        
+        var url = window.location.pathname;
+        var filename = url.substring(url.lastIndexOf('/') + 1);
+        
+        $("a").each(function () {
+
+            var href = $(this).attr("href");
+//            console.log(href);
+            
+            if (filename == href) {
+                $(this).parent().addClass("current");
+                
+                
+                try {
+                    var parentValue = $(this).parent().parent().attr("class");
+                    
+                    if (parentValue == 'dropdown') {
+                        $(this).parent().parent().parent().addClass("current")
+                    }
+                    
+                } catch (err) {}
+                
+                
+            }
+
+        });
+    });
+    
+
+    
+    
+});
