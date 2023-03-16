059e04022d42bfc239c89e0fc303e962f43ba118
SHUTitDOWN <SHUTitDOWN1998@gmail.com>
2023-03-15 19:57:06 -0500

    Update pageLoad.js

--- docs/js/pageLoad.js
+++ docs/js/pageLoad.js
@@ -1,50 +1,46 @@
 $().ready(function () {
 
 
-    //IMPORT FOOTER
-    $.ajax({
-        type: "GET",
-        url: "./template/websiteLogo.html",
-        async: false,
-        success: function(text) {
-            $("head").append(text);
-        }
-    })
-    $("footer").load("./template/footer.html", function() {
-        var year = new Date().getFullYear();
-        $(".currentYear").html(year);
-     });
-    
-    $("nav").load("./template/nav.html", function () {
-        
-        var url = window.location.pathname;
-        var filename = url.substring(url.lastIndexOf('/') + 1);
-        
-        $("a").each(function () {
+	//IMPORT FOOTER
+	$.ajax({
+		type: "GET",
+		url: "./template/websiteLogo.html",
+		async: false,
+		success: function(text) {
+			$("head").append(text);
+		}
+	})
+	$("footer").load("./template/footer.html", function() {
+		var year = new Date().getFullYear();
+		$(".currentYear").html(year);
+	 });
+	
+	$("nav").load("./template/nav.html", function () {
+		
+		var url = window.location.pathname;
+		var filename = url.substring(url.lastIndexOf('/') + 1);
+		
+		$("a").each(function () {
 
-            var href = $(this).attr("href");
+			var href = $(this).attr("href");
 //            console.log(href);
-            
-            if (filename == href) {
-                $(this).parent().addClass("current");
-                
-                
-                try {
-                    var parentValue = $(this).parent().parent().attr("class");
-                    
-                    if (parentValue == 'dropdown') {
-                        $(this).parent().parent().parent().addClass("current")
-                    }
-                    
-                } catch (err) {}
-                
-                
-            }
+			
+			if (filename == href) {
+				$(this).parent().addClass("current");
+				
+				
+				try {
+					var parentValue = $(this).parent().parent().attr("class");
+					
+					if (parentValue == 'dropdown') {
+						$(this).parent().parent().parent().addClass("current")
+					}
+					
+				} catch (err) {}
+				
+				
+			}
 
-        });
-    });
-    
-
-    
-    
+		});
+	});
 });
